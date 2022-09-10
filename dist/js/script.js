44f270d50292db07bf2302ff1e637e64bcda8e87
lac0220 <nemeth0220@gmail.com>
2022-09-10 02:08:15 +0200

    Add updated CV - Fix mobile navbar in JS

--- dist/js/script.js
+++ dist/js/script.js
@@ -16,6 +16,24 @@ window.addEventListener("load", () => {
     })
 }) 
 
+// nav menu opens
+const dropBar = document.querySelector(".drop-bar");
+const navMenu = document.querySelector("#nav-menu");
+
+const mobileMenu = () => {
+    dropBar.classList.toggle("change");
+    navMenu.classList.toggle("active");
+}
+dropBar.addEventListener("click", mobileMenu);
+
+// nav menu closes
+const navLinks = document.querySelector("#nav-menu");
+
+const mobileClose = () => {
+    dropBar.classList.toggle("change");
+    navMenu.classList.remove("active");
+}
+navLinks.addEventListener("click", mobileClose);
 
 /******/ })()
 ;
