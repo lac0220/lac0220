e58f44f61044c31d7d111d7b4a9990cb7e543f36
lac0220 <nemeth0220@gmail.com>
2022-09-25 02:11:21 +0200

    Add visitor counter to JS file

--- dist/js/script.js
+++ dist/js/script.js
@@ -16,6 +16,19 @@ window.addEventListener("load", () => {
     })
 }) 
 
+// counting number of visitors
+const visitorsNumber = document.getElementById('visitors-number');
+
+counting();
+
+function counting() {
+	fetch('https://api.countapi.xyz/update/lac0220/ce2d2945-acf3-4d7a-b45c-8f78b0e5ae92/?amount=1')
+	.then(result => result.json())
+	.then(result => {
+		visitorsNumber.innerHTML = result.value;
+	})
+}
+
 // nav menu opens
 const dropBar = document.querySelector(".drop-bar");
 const navMenu = document.querySelector("#nav-menu");
@@ -34,7 +47,6 @@ const mobileClose = () => {
     navMenu.classList.remove("active");
 }
 navLinks.addEventListener("click", mobileClose);
-
 /******/ })()
 ;
 //# sourceMappingURL=script.js.map
